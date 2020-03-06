68d064a1ae4a89e8e302fbca8978847f24519ea9
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-06 02:31:47 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require 'pry'
+
+class CashRegister 
+  attr_accessor :total, :discount, :items
+ 
+ def initialize(discount = 0) 
+  @total = 0
+  @discount = discount
+  @items = []
+ end
+ 
+ 
+
+def add_item(title, price, quantity = 1)
+  @total += price * quantity
+    quantity.times do
+      @items << title
+    end
+    @last_item = price * quantity
+  end
+
+
+def apply_discount
+  #here you want to subtract the discount from the total
+   if @discount > 0
+  disc_total = (@total * (discount.to_f/100.to_f))
+  @total -= disc_total
+  return "After the discount, the total comes to $#{@total.to_i}."
+  else
+    "There is no discount to apply."
+  end
+end
+
+def void_last_transaction
+  #want to void the last item added to the total
+  @total -= @last_item
+end
+end
+
+
 
