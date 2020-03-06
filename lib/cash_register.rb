5a055039d19d264279103ba5e61a2842d4d48bdb
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-06 02:00:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount :item
  
  def initialize(discount = 0) 
   @total = 0
@@ -24,10 +24,6 @@ def apply_discount
   return "After the discount, the total comes to $#{@total.to_i}."
   else
     "There is no discount to apply."
- end
- 
- def item
-   @item 
  end
 end
 end
