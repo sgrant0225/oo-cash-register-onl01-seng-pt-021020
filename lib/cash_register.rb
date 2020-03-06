733254c80573001a2492d45390d15850a75a2b2e
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-06 01:14:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ end
 
 def apply_discount
   #here you want to subtract the discount from the total
-   if @discount < 0
-  updated_total = 
+   if @discount > 0
+  updated_total = price * quantity
  end
 end
