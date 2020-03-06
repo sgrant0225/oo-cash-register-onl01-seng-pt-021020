515a0e5cb5d7c5113976b7308429563ef0696651
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-06 00:41:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
 
+class CashRegister 
+  attr_accessor :total, :discount
+ 
+ def initialize(discount = 0) 
+  @total = 0
+  @discount = discount
+ end
+ 
+ 
+
+def add_item(title, price, quantity = 1)
+  @total += price * quantity
+end  
+end
+
+def apply_discount
+  
+end
