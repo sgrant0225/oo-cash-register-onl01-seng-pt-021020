39766a8ba7bb3851120629c9aa4274d3bdee8d15
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-06 01:15:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ end
 def apply_discount
   #here you want to subtract the discount from the total
    if @discount > 0
-  updated_total = price * discount
+  updated_total = (price * discount)/100
   binding.pry
  end
 end
